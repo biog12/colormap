25398329b3451e4b98a03bbb7a4912a87e3c7065
biog12 <biog12@qq.com>
2017-07-15 16:10:17 +0800

    file output
    file output

--- map.py
+++ map.py
@@ -1,19 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 15 15:05:17 2017
+
+@author: vigh
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pylab as mtp
 
-t=pd.read_csv('D140321F003_HRB17-140315M-14.csv',skiprows=11)
-x = t['XADR']
-y = t['YADR']
-vf=t['VF1']
-iv=t['IV']
-wld=t['WD']
-ir=t['IR']
-#get data from file
-
-#定义mapping函数
 def map_x(x,y,z,vmin,vmax,step):
     vmin=vmin
     vmax=vmax
@@ -24,17 +20,36 @@ def map_x(x,y,z,vmin,vmax,step):
     bounds = np.arange(vmin,vmax+step,step)
     plt.scatter(x,y,c=z,cmap = cmap,s=100,marker=',',vmin=vmin,vmax=vmax)
     plt.colorbar(extend='both',ticks=bounds)
-#画图    
-plt.figure(figsize=(10,10))
-mtp.subplot(2,2,1)
-map_x(x,y,vf,2,3,0.1)
-mtp.title("VF")
-mtp.subplot(2,2,2)
-map_x(x,y,iv,500,550,5)
-mtp.title("IV")
-mtp.subplot(2,2,3)
-map_x(x,y,wld,450,465,1)
-mtp.title("WLD")
-mtp.subplot(2,2,4)
-map_x(x,y,ir,0,1,0.5)
-mtp.title("IR")
+    
+
+def map_a(path):
+    t=pd.read_csv(path,skiprows=11)
+    x = t['XADR']
+    y = t['YADR']
+    vf=t['VF1']
+    iv=t['IV']
+    wld=t['WD']
+    ir=t['IR'] 
+    plt.figure(figsize=(10,10))
+    mtp.subplot(2,2,1)
+    map_x(x,y,vf,2,3,0.1)
+    mtp.title("VF")
+    mtp.subplot(2,2,2)
+    map_x(x,y,iv,500,550,5)
+    mtp.title("IV")
+    mtp.subplot(2,2,3)
+    map_x(x,y,wld,450,465,1)
+    mtp.title("WLD")
+    mtp.subplot(2,2,4)
+    map_x(x,y,ir,0,1,0.5)
+    mtp.title("IR")
+    plt.savefig(os.path.splitext(list[i])[0]+".png")
+ 
+rootdir = 'D:\\123'
+list = os.listdir(rootdir)
+for i in range(0,len(list)):
+      path = os.path.join(rootdir,list[i])
+      map_a(path) 
+      
+      
+      
